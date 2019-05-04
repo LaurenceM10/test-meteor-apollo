97be47393f49baa5e281508e3795868eec6f13c9
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-05-04 09:08:43 -0600

    Refactor graphql server in server/server.js

--- server/server.js
+++ server/server.js
@@ -1,61 +1,41 @@
-import { ApolloServer, gql } from 'apollo-server-express'
-import { WebApp } from 'meteor/webapp'
-import { getUser } from 'meteor/apollo'
+import { ApolloServer, gql } from 'apollo-server-express';
+import { WebApp } from 'meteor/webapp';
 
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
-]
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
+  type Notes {
     title: String
-    author: String
+    description: String
+    accountId: Int
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    listNotes: [Notes]
   }
-`
+`;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: (x, y, { user }) => console.log(user) || books
-  }
-}
+    listNotes: (parent, args, context, info) => {
+      return [];
+    }
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => ({
-    user: await getUser(req.headers.authorization)
-  })
-})
+});
+
 
 server.applyMiddleware({
   app: WebApp.connectHandlers,
   path: '/graphql'
-})
+});
 
-// We are doing this work-around because Playground sets headers and WebApp also sets headers
-// Resulting into a conflict and a server side exception of "Headers already sent"
+
 WebApp.connectHandlers.use('/graphql', (req, res) => {
   if (req.method === 'GET') {
-    res.end()
+    res.end();
   }
-})
+});
+
